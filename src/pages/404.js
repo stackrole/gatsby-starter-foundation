028d23aef20acba38f9a4dbd54f27aa0e8f974f4
Stackrole Maker <64824192+stackrole-dev@users.noreply.github.com>
2020-05-24 23:08:59 +0530

    404, responsive, git gateway

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,10 +1,27 @@
 import React from "react"
+import { Link } from "gatsby"
+import {RiArrowLeftSLine, RiBugLine, RiSkullLine} from "react-icons/ri"
 
+import SEO from "../components/seo"
 import Layout from "../components/layout"
 
 const NotFound = () => (
-  <Layout>
-    <h1>Page not found</h1>
+  <Layout className="not-found-page">
+    <SEO title="Page not found"/>
+    <div className="wrapper" style={{
+      textAlign: "center"
+    }}>
+      <header>
+        <RiSkullLine style={{
+          fontSize: "128px",
+          color: "var(--primary-color)"
+        }}/>
+        <h1>Oops we did not expect that to happen</h1>
+        <p>Have you wondered into the unknow. Let us help you, Please take a look at below options</p>
+      </header>
+      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Back to Homepage</Link>
+      <Link to="/contact" className="button -outline">Report this <RiBugLine className="icon -right"/></Link>
+    </div>
   </Layout>
 )
 
--- src/pages/thanks.js
+++ src/pages/thanks.js
@@ -1,9 +1,26 @@
-import React from 'react'
-import Layout from '../components/layout'
+import React from "react"
+import { Link } from "gatsby"
+import {RiArrowLeftSLine, RiCheckboxCircleLine} from "react-icons/ri"
 
-export default () => (
-  <Layout>
-    <h1>Thank you!</h1>
-    <p>This is a custom thank you page for form submissions</p>
+import SEO from "../components/seo"
+import Layout from "../components/layout"
+
+const Thanks = () => (
+  <Layout className="thanks-page">
+    <SEO title="Thank you"/>
+    <div className="wrapper" style={{
+      textAlign: "center"
+    }}>
+      <RiCheckboxCircleLine style={{
+        fontSize: "128px",
+        color: "var(--primary-color)"
+      }}/>
+      <h1>Got your message</h1>
+      <p>Thank you for getting in touch us. We will get back to you shortly.</p>
+      <Link to="/" className="button"><RiArrowLeftSLine className="icon -left"/>Lets go back to Homepage</Link>
+    </div>
+
   </Layout>
 )
+
+export default Thanks
