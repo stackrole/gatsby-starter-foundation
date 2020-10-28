d28ca04de5e81d3a6dbc10b3fa6f8f2b763c32b8
Sameer Ahamed <41252589+sksameer204@users.noreply.github.com>
2020-10-28 16:47:51 +0530

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -85,7 +85,7 @@ module.exports = {
         background_color: `#f7f0eb`,
         theme_color: `#a2466c`,
         display: `standalone`,
-        icon: `static/assets/stackrole.png`,
+        icon: "static" + settings.meta.iconimage,
       },
     },
     'gatsby-plugin-offline',
