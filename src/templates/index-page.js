a0f1f6cdfa81bad39709c56039da1ed37ed0da77
Munawar Ahmed <munwr@users.noreply.github.com>
2022-04-22 07:36:44 +0530

    Adding "noopener noreferrer" to external links

--- src/templates/index-page.js
+++ src/templates/index-page.js
@@ -80,119 +80,119 @@ const HomePage = ({ data }) => {
     return (
       <div key={"social icons" + index}>
         {icons.icon === "facebook" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Facebook">
+          <a href={icons.url} target="_blank" aria-label="link to Facebook" rel="noopener noreferrer">
             <RiFacebookBoxFill alt="Facebook icon"/>
           </a>
         ) : (
           ""
         )}
         {icons.icon === "twitter" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Twitter">
+          <a href={icons.url} target="_blank" aria-label="link to Twitter" rel="noopener noreferrer">
             <RiTwitterFill alt="Twitter icon"/>
           </a>
         ) : (
           ""
         )}
         {icons.icon === "linkedin" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Linkedin">
+          <a href={icons.url} target="_blank" aria-label="link to Linkedin" rel="noopener noreferrer">
             <RiLinkedinBoxFill alt="Linkedin icon"/>
           </a>
         ) : (
           ""
         )}
         {icons.icon === "youtube" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Youtube">
+          <a href={icons.url} target="_blank" aria-label="link to Youtube" rel="noopener noreferrer">
             <RiYoutubeFill alt="Youtube icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "instagram" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Instagram">
+          <a href={icons.url} target="_blank" aria-label="link to Instagram" rel="noopener noreferrer">
             <RiInstagramFill alt="Instagram icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "rss" ? (
-          <a href={icons.url} target="_blank" aria-label="link to RSS" >
+          <a href={icons.url} target="_blank" aria-label="link to RSS" rel="noopener noreferrer">
             <RiRssFill alt="RSS icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "github" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Github" >
+          <a href={icons.url} target="_blank" aria-label="link to Github" rel="noopener noreferrer">
             <RiGithubFill alt="Github icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "telegram" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Telegram" >
+          <a href={icons.url} target="_blank" aria-label="link to Telegram" rel="noopener noreferrer">
             <RiTelegramFill alt="Telegram icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "pinterest" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Pinterest">
+          <a href={icons.url} target="_blank" aria-label="link to Pinterest" rel="noopener noreferrer">
             <RiPinterestFill alt="Pinterest icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "snapchat" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Snapchat">
+          <a href={icons.url} target="_blank" aria-label="link to Snapchat" rel="noopener noreferrer">
             <RiSnapchatFill alt="Snapchat icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "skype" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Skype">
+          <a href={icons.url} target="_blank" aria-label="link to Skype" rel="noopener noreferrer">
             <RiSkypeFill alt="Skype icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "wordpress" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Wordpress">
+          <a href={icons.url} target="_blank" aria-label="link to Wordpress" rel="noopener noreferrer">
             <FaWordpress alt="Wordpress icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "tiktok" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Wordpress">
+          <a href={icons.url} target="_blank" aria-label="link to Wordpress" rel="noopener noreferrer">
             <FaTiktok alt="tiktok icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "dribbble" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Dribbble">
+          <a href={icons.url} target="_blank" aria-label="link to Dribbble" rel="noopener noreferrer">
             <RiDribbbleFill alt="Dribbble icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "medium" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Medium">
+          <a href={icons.url} target="_blank" aria-label="link to Medium" rel="noopener noreferrer">
             <RiMediumFill alt="Medium icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "behance" ? (
-          <a href={icons.url} target="_blank" aria-label="link to Behance">
+          <a href={icons.url} target="_blank" aria-label="link to Behance" rel="noopener noreferrer">
             <RiBehanceFill alt="Behance icon" />
           </a>
         ) : (
           ""
         )}
         {icons.icon === "vk" ? (
-          <a href={icons.url} target="_blank" aria-label="link to vk">
+          <a href={icons.url} target="_blank" aria-label="link to vk" rel="noopener noreferrer">
             <FaVk alt="vk icon" />
           </a>
         ) : (
